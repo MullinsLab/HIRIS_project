1b057cb9a704e19d21e81f005c73ab7a0bbfcb0b
PeterDarley <52110396+PeterDarley@users.noreply.github.com>
2023-02-15 17:59:01 +0000

    Set up login

--- hiris/apps/core/views/tool_views.py
+++ hiris/apps/core/views/tool_views.py
@@ -1,6 +1,26 @@
+import logging
+logger = logging.getLogger('app')
+
 from django.views.generic.base import View
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
-class About(View):
+class Home(View):
+    ''' The default view for HIRIS Home.  Currently shows the About page '''
     def get(self, request, *args, **kwargs):
+        ''' Returns the default template on a get '''
+        return render(request, "about.django-html")
+
+
+    def post(self, request, *args, **kwargs):
+        ''' Recieves the username and password for login '''
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+        # logger.info(request.POST)
+
         return render(request, "about.django-html")
