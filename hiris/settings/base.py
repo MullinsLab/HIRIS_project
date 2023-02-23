020fda141e61e4d0c61087ef53f4d46c3902dc57
PeterDarley <52110396+PeterDarley@users.noreply.github.com>
2023-02-23 15:32:26 +0000

    Configured Whitenoise

--- hiris/settings/base.py
+++ hiris/settings/base.py
@@ -143,8 +143,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     PROJECT_DIR / 'static/',
 ]
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-# STATIC_ROOT = os.path.join(BASE_DIR,"assets")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
