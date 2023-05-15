53dc474a0b7db93da80e4b041af9ea5a0ddb8431
PeterDarley <52110396+PeterDarley@users.noreply.github.com>
2023-05-15 14:24:37 +0000

    Fixed problem when certs dir wasn't set in .env

--- hiris/settings/third_party.py
+++ hiris/settings/third_party.py
@@ -1,14 +1,10 @@
 import os
 from .base import env
-# from hiris.apps.core.models import GenomeSpecies
 
-# # For UW_SAML
-# from django.urls import reverse_lazy
-# #LOGIN_URL = reverse_lazy('saml_login')
-# LOGIN_URL = reverse_lazy('login')
-# LOGIN_REDIRECT_URL = "/"
+# For UW_SAML
 
-if env('UW_SAML_CERT_DIR'):
+#if env('UW_SAML_CERT_DIR'):
+if 'UW_SAML_CERT_DIR' in env:
     UW_SAML_CERT_DIR = env('UW_SAML_CERT_DIR')
 
     UW_SAML_PRIVATE_KEY = env('UW_SAML_PRIVATE_KEY')
@@ -23,43 +19,43 @@ if env('UW_SAML_CERT_DIR'):
     with open(os.path.join(UW_SAML_CERT_DIR, UW_SAML_IDPCERT), "r") as file:
         UW_SAML_IDPCERT = file.read()
 
-UW_SAML = {
-    'strict': False,
-    'debug': True,
-    'sp': {
-        'entityId': 'https://dev.hiris.washington.edu',
-        'assertionConsumerService': {
-            'url': 'https://dev.hiris.washington.edu/saml/sso',
-            'binding': 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST'
+    UW_SAML = {
+        'strict': False,
+        'debug': True,
+        'sp': {
+            'entityId': 'https://dev.hiris.washington.edu',
+            'assertionConsumerService': {
+                'url': 'https://dev.hiris.washington.edu/saml/sso',
+                'binding': 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST'
+            },
+            'singleLogoutService': {
+                'url': 'https://dev.hiris.washington.edu/saml/logout',
+                'binding': 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect'
+            },
+            'NameIDFormat': 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified',
+            'x509cert': UW_SAML_PUBLIC_CERT,
+            # for encrypted saml assertions uncomment and add the private key
+            'privateKey': UW_SAML_PRIVATE_KEY,
         },
-        'singleLogoutService': {
-            'url': 'https://dev.hiris.washington.edu/saml/logout',
-            'binding': 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect'
+        'idp': {
+            'entityId': 'urn:mace:incommon:washington.edu',
+            'singleSignOnService': {
+                'url': 'https://idp.u.washington.edu/idp/profile/SAML2/Redirect/SSO',
+                'binding': 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect'
+            },
+            'singleLogoutService': {
+                'url': 'https://idp.u.washington.edu/idp/logout',
+                'binding': 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect'
+            },
+            'x509cert': UW_SAML_IDPCERT,
         },
-        'NameIDFormat': 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified',
-        'x509cert': UW_SAML_PUBLIC_CERT,
-        # for encrypted saml assertions uncomment and add the private key
-        'privateKey': UW_SAML_PRIVATE_KEY,
-    },
-    'idp': {
-        'entityId': 'urn:mace:incommon:washington.edu',
-        'singleSignOnService': {
-            'url': 'https://idp.u.washington.edu/idp/profile/SAML2/Redirect/SSO',
-            'binding': 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect'
-        },
-        'singleLogoutService': {
-            'url': 'https://idp.u.washington.edu/idp/logout',
-            'binding': 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect'
-        },
-        'x509cert': UW_SAML_IDPCERT,
-    },
-    'security': {
-        # for encrypted saml assertions
-        'wantAssertionsEncrypted': True,
-        # for 2FA uncomment this line
-        # 'requestedAuthnContext':  ['urn:oasis:names:tc:SAML:2.0:ac:classes:TimeSyncToken']
+        'security': {
+            # for encrypted saml assertions
+            'wantAssertionsEncrypted': True,
+            # for 2FA uncomment this line
+            # 'requestedAuthnContext':  ['urn:oasis:names:tc:SAML:2.0:ac:classes:TimeSyncToken']
+        }
     }
-}
 
 # Mock login for testing
 # DJANGO_LOGIN_MOCK_SAML = {
