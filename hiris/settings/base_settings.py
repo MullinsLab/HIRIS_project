d89dd44eb0c9c3613b4a9f0006899626c253c28c
PeterDarley <52110396+PeterDarley@users.noreply.github.com>
2024-04-16 19:17:31 +0000

    Removed unneeded setting

--- hiris/settings/base_settings.py
+++ hiris/settings/base_settings.py
@@ -1,21 +1,16 @@
-import os #,sys
-import environ
-from pathlib import Path
-from django.conf.urls.static import static
 import logging
 import logging.config
-from django.utils.log import DEFAULT_LOGGING
+
+
+import environ
+import os #,sys
+from pathlib import Path
+
+from django.conf.urls.static import static
 from django.urls import reverse_lazy
+from django.utils.log import DEFAULT_LOGGING
 
 from . import BASE_DIR, PROJECT_DIR
-
-### These settings have been moved to __init__.py
-
-    # Build paths inside the project like this: BASE_DIR / 'subdir'.
-    # BASE_DIR = Path(__file__).resolve().parent.parent.parent
-    # PROJECT_DIR = Path(__file__).resolve().parent.parent
-
-### End moved settings
 
 
 # Take environment variables from .env file
